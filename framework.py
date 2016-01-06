44a10826a4845e8b40d7e1712cb403eca48c415f
Michael Klopot <michael.klopot@gmail.com>
2016-01-05 20:29:03 -0700

    Add a rot_center() function to rotate images

--- framework.py
+++ framework.py
@@ -28,12 +28,12 @@ def init():
     
     global space
     space = pymunk.Space()
-    space.collision_bias = pow(1.0 - 0.3, 60.0)
+    space.collision_bias = pow(1.0 - 0.3, 70.0)
     space.gravity = (0.0, 900.0)
-    space.iterations = 4
+    space.iterations = 5
     #space.idle_speed_threshold = 20
-    space.collision_slop = .001
-    space.damping = .95
+    space.collision_slop = .0001
+    space.damping = .99
 
     global scrolling
     scrolling = pymunk.Vec2d(0,0)
@@ -54,3 +54,12 @@ def init():
     
     global grabbables
     grabbables = []
+
+def rot_center(image, angle):
+    """rotate an image while keeping its center and size"""
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
