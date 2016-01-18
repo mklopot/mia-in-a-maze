4e187a8606f99766eefd87e19124956adfa6f9b9
Michael Klopot <michael.klopot@gmail.com>
2016-01-18 11:11:41 -0700

    Double-buffered fullscreen display

--- framework.py
+++ framework.py
@@ -23,7 +23,8 @@ def init():
     global screen
 #    screen = pygame.display.set_mode((viewport_width, viewport_height))
 #    screen = pygame.display.set_mode((viewport_width, viewport_height),pygame.FULLSCREEN | pygame.HWSURFACE)
-    screen = pygame.display.set_mode((viewport_width, viewport_height),pygame.FULLSCREEN)
+    screen = pygame.display.set_mode((viewport_width, viewport_height),pygame.FULLSCREEN | pygame.DOUBLEBUF)
+#    screen = pygame.display.set_mode((viewport_width, viewport_height),pygame.FULLSCREEN)
     pygame.display.set_caption("Mia in a Maze")
     pygame.key.set_repeat(10)
     
